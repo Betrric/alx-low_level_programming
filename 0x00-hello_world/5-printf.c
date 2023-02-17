4d6283e8ed12b271fa5bc4fdcb136a5aab1f2ecf
Betrric <beatricewambui1998@gmail.com>
2023-02-16 22:55:36 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+ * main - A program that uses the function of printf
+ * Return:0 if success
+ */
+
+int main(0)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
